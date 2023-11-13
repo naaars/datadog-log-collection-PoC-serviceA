a11fcb22bf339e9b53a793a81ed00fd70432afec
Inigo Moreno <inigomorenotato@gmail.com>
2023-11-13 10:39:05 +0100

    Added autopolling to datadogAService

--- src/main/java/de/santander/poc/datadogServiceA/DatadogServiceAApplication.java
+++ src/main/java/de/santander/poc/datadogServiceA/DatadogServiceAApplication.java
@@ -1,13 +1,38 @@
 package de.santander.poc.datadogServiceA;
 
+import de.santander.poc.datadogServiceA.client.LongPollingDatadogAClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 
 @SpringBootApplication
 public class DatadogServiceAApplication {
 
+	@Value("${datadogserviceb.base.url}")
+	private static long datadogServiceBMillis;
+
+	private static LongPollingDatadogAClient longPollingDatadogAClient = new LongPollingDatadogAClient();
+
+	private static final Logger logger = LogManager.getLogger(DatadogServiceAApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(DatadogServiceAApplication.class, args);
+		boolean whileControl = true;
+
+		while(whileControl) {
+			try {
+				Thread.sleep(datadogServiceBMillis);
+				logger.info("Calling DatadogAService from Long Polling Client");
+				logger.info("Response from DatadogAService: {}", longPollingDatadogAClient.callDatadogAWithRestTemplate(new RestTemplateBuilder()));
+			} catch (InterruptedException e) {
+				logger.error("Error calling DatadogBService: {}", e.getMessage());
+				whileControl = false;
+			}
+
+		}
 	}
 
 }
--- src/main/java/de/santander/poc/datadogServiceA/client/LongPollingDatadogAClient.java
+++ src/main/java/de/santander/poc/datadogServiceA/client/LongPollingDatadogAClient.java
@@ -4,27 +4,26 @@ import de.santander.poc.datadogServiceA.service.DatadogAService;
 import io.netty.handler.timeout.ReadTimeoutException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Duration;
 
 @Component
 public class LongPollingDatadogAClient {
-
     private static final Logger logger = LogManager.getLogger(LongPollingDatadogAClient.class);
-
-    public String callBakeWithWebClient() {
-        WebClient webClient = WebClient.create();
+    public String callDatadogAWithRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+        RestTemplate restTemplate = restTemplateBuilder
+                .setConnectTimeout(Duration.ofSeconds(10))
+                .setReadTimeout(Duration.ofSeconds(10))
+                .build();
 
         try {
-            return webClient.get()
-                    .uri("/api/hello")
-                    .retrieve()
-                    .bodyToFlux(String.class)
-                    .timeout(Duration.ofSeconds(10))
-                    .blockFirst();
-        } catch (ReadTimeoutException e) {
+            return restTemplate.getForObject("http://localhost:8080/datadog-service-A/api/hello", String.class);
+        } catch (ResourceAccessException e) {
             logger.error("Error calling DatadogAService: {}", e.getMessage());
             throw e;
         }
